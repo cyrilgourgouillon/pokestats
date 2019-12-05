6fd4cd43ee8f5c6f8fc27a3bf5816a2514a7eea8
Cyril Gourgouillon <cyrilgour@gmail.com>
2019-12-05 15:49:21 +0100

    add type #7

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function printItem(param) {
             printColor(param);
             break;
         case ("nature de l'élément"): //instance of
-            //printInstanceOf(param); 
+            printInstanceOf(param);
             break;
         case ("partie de"): //part of
             //printPartOf(param); 
@@ -77,12 +77,14 @@ function printColor(parameter) {
             //Print the error
             console.error(error);
         } else {
-            console.log(response.data);
 
             $("#parameters-form").append(`
                 <div class="form-group">
                     <label class="form-check-label mb-2">Couleur(s)</label>
-                    <div class="form-check">
+                    <button class="btn btn-secondary btn-sm" type="button" data-toggle="collapse" data-target="#collapseColor" aria-expanded="false" aria-controls="collapseColor">
+                        <i class="fas fa-angle-down"></i>
+                    </button>
+                    <div class="form-check collapse pt-1" id="collapseColor">
                         ${getColorsHTML(response.data)}
                     </div>
                 </div>
@@ -107,6 +109,74 @@ function getColorHTML(color) {
             <span class="badge badge-count badge-danger">${color.count}</span>
         </div>
     `;
+}
+
+function printInstanceOf(parameter) {
+    $.get("api/getParameterValues", {
+        reference: parameter.reference
+    }, (response) => {
+        if (response.error) {
+            //Print the error
+            console.error(error);
+        } else {
+            console.log(response.data);
+            checkInstanceOf(response.data);
+        }
+    });
+}
+
+function checkInstanceOf(instance) {
+
+    let allTypes;
+
+    allTypes = instance.filter(i => i.name.startsWith("Pokémon de type "));
+
+
+    printType(allTypes);
+
+}
+
+function printType(allTypes) {
+
+    $("#parameters-form").append(`
+    <div class="form-group">
+        <div class="form-group">
+            <label class="form-check-label mb-2">Type(s)</label>
+            <button class="btn btn-secondary btn-sm" type="button" data-toggle="collapse" data-target="#collapseType" aria-expanded="false" aria-controls="collapseType">
+                <i class="fas fa-angle-down"></i>
+            </button>
+            <div class="form-check collapse pt-1" id="collapseType">
+                ${getTypesHTML(allTypes)}
+            </div>
+        </div>
+    </div>`);
+}
+
+function getTypesHTML(types) {
+    return types.reduce((acc, type) => acc += getTypeHTML(type), "");
+}
+
+function getTypeHTML(type) {
+
+    let typeName = type.name.substring(16);
+
+    let typeNoAccent = removeAccent(typeName);
+
+    return `
+    <div>
+        <label for="pokemon-type-${typeNoAccent}" class="btn btn-sm text-white rounded-pill btn-badge-text type-${typeNoAccent}">${typeName} 
+            <input type="checkbox" id="pokemon-type-${typeNoAccent}" value="${typeNoAccent}" class="badgebox">
+            <span class="badge badge-white text-primary">
+                <i class="fa fa-check"></i>
+            </span>     
+        </label>
+        <span class="badge badge-count badge-danger">${type.count}</span>
+    </div>
+    `;
+}
+
+function removeAccent(string) {
+    return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
 function printString(param) {
